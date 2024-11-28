7deabc588c80ba7a372eae186ef45fc15dda9ca0
haneefMuhammad <haneefmhmmd@gmail.com>
2024-11-28 12:28:21 -0500

    added /api prefix

--- medLab/Controllers/ReportsController.cs
+++ medLab/Controllers/ReportsController.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Authorization;
 namespace medLab.Controllers
 {
     [ApiController]
-    [Route("report")]
+    [Route("api/report")]
     [Authorize]
     public class ReportsController : ControllerBase
     {
